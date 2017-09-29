2040a3b619f0a0b87b08ebf8aa64c7b3e40bec7d
Jivago Medeiros <Jivago@ic.ufmt.br>
2017-09-28 20:19:18 -0400

    adiciona comentários no exemplo de análise descritiva do dados do arquivo dados.csv

--- exemplos/lendo_dados_csv_analise_descritiva.R
+++ exemplos/lendo_dados_csv_analise_descritiva.R
@@ -1,9 +1,11 @@
 planilha=read.csv('dados.csv',header=TRUE,sep=',',dec='.')
 
-temperatura = planilha$T_z1
+temperatura = planilha$T_z1    #extraindo um vetor de dados (temperatura) a partir da coluna T_z1 
 
-minT=min(temperatura)
-maxT=max(temperatura)
-meanT=mean(temperatura)
-sdT=sd(temperatura)
-medianaT=median(temperatura)
+minT      = min(temperatura)  #valor mínimo no vetor temperatura
+maxT      = max(temperatura)  #valor máximo no vetor temperatura
+meanT     = mean(temperatura) #média de valores no vetor temperatura
+sdT       = sd(temperatura)   #desvio padrão dos valores no vetor temperatura
+medianaT  = median(temperatura) #mediana dos valores no vetor temperatura
+
+quantidade = length(temperatura[temperatura>meanT]) #temperaturas maiores que a média
